76ca01a9617ec9fa42f08f672832c95c4149cc48
neha01kbizsoft <neha.kbizsoft@gmail.com>
2023-11-24 12:19:52 +0530

    testing

--- assets/javascripts/discourse/components/ad-slot.js
+++ assets/javascripts/discourse/components/ad-slot.js
@@ -262,9 +262,6 @@ export default AdComponent.extend({
     return networkNames;
   },
 
-  addExcoPlayer() {
-    const currentUser = this.get("currentUser");
-    var valueExists = true;
-    console.log(valueExists);
-}
+
+
 });
